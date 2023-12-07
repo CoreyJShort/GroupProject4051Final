d21b3487e309037159288b30de93ebc3a9bec8a7
Corey Short <corey.short@marquette.edu>
2023-12-06 18:48:49 -0600

    trying to merge to master

---
+++ GroupProject4051Final/Model/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject4051Final.Model;
+
+internal class Customer
+{ 
+    public int Id { get; set; } // Unique identifier for the customer
+    public string FirstName { get; set; } // First name of the customer
+    public string LastName { get; set; } // Last name of the customer
+    public string UserName { get; set; } // Username for authentication
+    public string Password { get; set; } // Password for authentication
+}
