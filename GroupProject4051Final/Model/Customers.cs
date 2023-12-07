20c21e4ec0496c0a680fc83f4f95c2a4c1f5a50d
Corey Short <corey.short@marquette.edu>
2023-12-06 18:01:53 -0600

    added customers

---
+++ GroupProject4051Final/Model/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject4051Final.Model;
+
+internal class Customers
+{
+    public List<Customer> CustomerList { get; set; } // List to store customer objects
+
+    // Constructor to initialize the CustomerList
+    public Customers()
+    {
+        CustomerList = new List<Customer>();
+    }
+
+    // Method to authenticate a customer based on username and password
+    public Customer Authenticate(string username, string password)
+    {
+        var c = CustomerList.FirstOrDefault(o => o.UserName == username && o.Password == password);
+
+        return c;
+    }
+}
