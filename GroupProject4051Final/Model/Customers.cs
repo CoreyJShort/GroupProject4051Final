a5285155c0e14c2f9668bbe1bc2cebd96e2bb370
Corey Short <corey.short@marquette.edu>
2023-12-06 18:23:15 -0600

    customers was input wrong

--- GroupProject4051Final/Model/Customers.cs
+++ GroupProject4051Final/Model/Customers.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GroupProject4051Final.Model;
-
-internal class Customers 
+internal class Customers
 {
     public List<Customer> CustomerList { get; set; } // List to store customer objects
 
@@ -21,6 +20,24 @@ internal class Customers
     {
         var c = CustomerList.FirstOrDefault(o => o.UserName == username && o.Password == password);
 
-        return c;
+        return c; // Returns authenticated customer or null if not found
+    }
+
+    // Method to add a new customer to the CustomerList
+    public void AddCustomer(string firstName, string lastName, string username, string password)
+    {
+        int newCustomerId = CustomerList.Count + 1; // Generate a new unique customer ID
+
+        var newCustomer = new Customer
+        {
+            Id = newCustomerId,
+            FirstName = firstName,
+            LastName = lastName,
+            UserName = username,
+            Password = password
+        };
+
+        CustomerList.Add(newCustomer); // Add the new customer to the list
+        Console.WriteLine($"User {username} added successfully!"); // Display a success message
     }
 }
